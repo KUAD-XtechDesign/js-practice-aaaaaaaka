8386d40c5df81aa04420db95843a287348dbac3a
aaaaaaaka <68629495+aaaaaaaka@users.noreply.github.com>
2020-10-23 13:34:06 +0900

    １０.２３

---
+++ sample/js/script02.js
@@ -0,0 +1,27 @@
+$(function(){
+  let txt1 ="やき"
+ 　let txt2 ="のり" 
+ 　let num1 =20;
+   let num2=32;
+    $("#button01").on("click",function(){
+      $("#content").text("こんにちは");
+    })
+ 
+    $("#button02").on("click",function(){
+     $("#content").text(txt1);
+    })
+    
+    $("#button03").on("click",function(){
+     $("#content").text(txt1+txt2);
+    })
+ 
+    $("#button04").on("click",function(){
+     $("#content").text(num1+num2);
+    })
+ 
+
+    $("#button05").on("click",function(){
+        $("#content").text(txt1+(num1num2)+"です");
+       })
+ 
+ })
